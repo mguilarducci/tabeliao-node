94ebdabaafbb2b2258c38d62e2c663ff583edee0
Matheus Guilarducci <mguilarducci@gmail.com>
2016-06-23 12:36:20 -0300

    Configura grunt eslint

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,14 @@
-module.exports = function(grunt) {
+var loadGruntTasks = require('load-grunt-tasks');
+var files = ['gruntfile.js'];
+
+module.exports = function gruntConfig(grunt) {
+  loadGruntTasks(grunt);
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    eslint: {
+      target: files
+    }
   });
 };
