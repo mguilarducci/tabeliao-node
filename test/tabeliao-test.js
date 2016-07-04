28ab535112894ca1601ace7997335cd5e3d7a1c8
Matheus Guilarducci <mguilarducci@gmail.com>
2016-07-04 14:42:05 -0300

    Registra serviços com consul
    Registra os servicos com consul, porem apenas com valores default

--- test/tabeliao-test.js
+++ test/tabeliao-test.js
@@ -1,6 +1,7 @@
 /* eslint no-underscore-dangle: ["error", { "allow": ["__set__"] }] */
 
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var rewire = require('rewire');
 var os = require('os');
 var pkgDir = __dirname + '/../package.json';
@@ -15,7 +16,20 @@ var project = {
   version: 'v' + pkg.version
 };
 
-tabeliao.__set__('PKG_DIR', pkgDir);
+var consul = {
+  agent: { service: { register: sinon.stub() } }
+};
+
+var serviceData = {
+  id: project.id,
+  name: project.name,
+  tags: [project.tech, project.version]
+};
+
+tabeliao.__set__({
+  consul: consul,
+  PKG_DIR: pkgDir
+});
 
 describe('Getting project data', function desc() {
   it('should return the name correctly', function test() {
@@ -37,5 +51,29 @@ describe('Getting project data', function desc() {
   it('should return the correct object', function test() {
     var allKeys = ['id', 'name', 'tags'];
     expect(tabeliao.getProjectData()).to.have.all.keys(allKeys);
+  });
+});
+
+describe('Calling consul agent', function desc() {
+  /* eslint no-unused-expressions: 0 */
+  beforeEach(function setUp() {
+    consul.agent.service.register.callsArgAsync(1);
+  });
+
+  it('should register the service', function test(done) {
+    tabeliao.register(function cb(err) {
+      expect(err).to.not.exist;
+      expect(consul.agent.service.register.calledOnce).to.be.true;
+      done();
+    });
+  });
+
+  it('should register the service with correct values', function test(done) {
+    tabeliao.register(function cb(err) {
+      var args = consul.agent.service.register.lastCall.args[0];
+      expect(err).to.not.exist;
+      expect(args).to.deep.equal(serviceData);
+      done();
+    });
   });
 });
