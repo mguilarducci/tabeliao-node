3c1b583016efaaf20701c1218282fa8b2188cb80
Matheus Guilarducci <mguilarducci@gmail.com>
2016-07-05 19:26:39 -0300

    Refatora funcoes de host

--- test/tabeliao-test.js
+++ test/tabeliao-test.js
@@ -68,10 +68,6 @@ describe('Getting project data', function desc() {
     expect(tabeliao.getServiceId(pkg)).to.be.equal(serviceData.id);
   });
 
-  it('should return correct version', function test() {
-    expect(tabeliao.getVersion(pkg)).to.be.equal(serviceData.tags[1]);
-  });
-
   it('should return correct port', function test() {
     expect(tabeliao.getPort(app)).to.be.equal('5000');
   });
@@ -135,5 +131,58 @@ describe('tabeliao.register', function desc() {
       expect(consul.agent.service.register.calledOnce).to.be.true;
       done();
     });
+  });
+});
+
+describe('tabeliao.getVersion', function desc() {
+  it('should return correct version', function test() {
+    expect(tabeliao.getVersion({ version: '1.2.3' })).to.be.equal('v1.2.3');
+    expect(tabeliao.getVersion({ version: '2.2.2' })).to.be.equal('v2.2.2');
+    expect(tabeliao.getVersion({ version: '0' })).to.be.equal('v0');
+  });
+});
+
+describe('tabeliao.getServiceId', function desc() {
+  it('should return correct service id', function test() {
+    expect(tabeliao.getServiceId({ name: '1.2.3' }))
+      .to.be.equal(os.hostname() + '-1.2.3');
+    expect(tabeliao.getServiceId({ name: '2.2.2' }))
+      .to.be.equal(os.hostname() + '-2.2.2');
+    expect(tabeliao.getServiceId({ name: '0' }))
+      .to.be.equal(os.hostname() + '-0');
+  });
+});
+
+describe('tabeliao.getPort', function desc() {
+  it('should return correct port', function test() {
+    expect(tabeliao.getPort({ port: '1234' })).to.be.equal('1234');
+    expect(tabeliao.getPort({ port: '2222' })).to.be.equal('2222');
+    expect(tabeliao.getPort({ port: '0000' })).to.be.equal('0000');
+  });
+
+  it('should return 3000 as default', function test() {
+    expect(tabeliao.getPort()).to.be.equal(3000);
+  });
+});
+
+describe('tabeliao.getHost', function desc() {
+  it('should return correct host', function test() {
+    expect(tabeliao.getHost({ host: 'abc.com' })).to.be.equal('abc.com');
+    expect(tabeliao.getHost({ host: 'example.com' })).to.be.equal('example.com');
+    expect(tabeliao.getHost({ host: 'pipitchu.xyz' })).to.be.equal('pipitchu.xyz');
+  });
+
+  it('should return localhost as default', function test() {
+    expect(tabeliao.getHost()).to.be.equal('localhost');
+  });
+});
+
+describe('tabeliao.getProtocol', function desc() {
+  it('should return https when ssl is true', function test() {
+    expect(tabeliao.getProtocol({ ssl: true })).to.be.equal('https');
+  });
+
+  it('should return http as default', function test() {
+    expect(tabeliao.getProtocol()).to.be.equal('http');
   });
 });
